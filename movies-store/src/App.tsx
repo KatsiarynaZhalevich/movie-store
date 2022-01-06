8bdb017375f42bbcfd6081c4065991e02e9ac7ca
KatsiarynaZhalevich <kzhalevich@merkleinc.com>
2022-01-06 17:58:52 +0300

    set up the project

---
+++ movies-store/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App(): JSX.Element {
+  console.log('d');
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
